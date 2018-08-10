e0577262f293066e5773bc2a3af2f441ab75f8bc
Joel Pitt <joel@joelpitt.com>
2018-08-10 16:12:28 +1200

    Initial checkin for text-summarization hacking

---
+++ text-summarization/fetch_corenlp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-01-31.zip
+unzip stanford-corenlp-full-2018-01-31.zip
+export CLASSPATH=stanford-corenlp-full-2018-01-31/stanford-corenlp-3.9.1.jar
+
+# command to test:
+# echo "Please tokenize this text." | java edu.stanford.nlp.process.PTBTokenizer -preserveLines
