a7f4de8574b6d57fe2e2a69ca33de9dc297aaca5
Glauter Lemos <glautervl@gmail.com>
2018-09-06 11:10:28 -0300

    Addning deployment container scripts

---
+++ sentiment_analysis/start_container_service.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker build -t singularitynet/sentiment_analysis:v1.0 https://raw.githubusercontent.com/singnet/nlp-services/sentiment_analysis/sentiment_analysis/Dockerfile
+docker run --name SENTIMENT_ANALYSIS -p 7010:7010 -idt singularitynet/sentiment_analysis_test:latest
+
