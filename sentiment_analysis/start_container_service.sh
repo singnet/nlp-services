7cdf8508e503f2a288940209538a1e56530da0aa
Glauter Lemos <glautervl@gmail.com>
2018-09-06 11:16:58 -0300

    Container deployment improvments

--- sentiment_analysis/start_container_service.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t singularitynet/sentiment_analysis:v1.0 https://raw.githubusercontent.com/singnet/nlp-services/sentiment_analysis/sentiment_analysis/Dockerfile &
-wait; docker run --name SENTIMENT_ANALYSIS -p 7010:7010 -idt singularitynet/sentiment_analysis_test:latest
-
