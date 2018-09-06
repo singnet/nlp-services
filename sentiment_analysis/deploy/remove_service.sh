8d2ae859e7f4c5b5ea58d0ffd9f7bf378b2e2b74
Glauter Lemos <glautervl@gmail.com>
2018-09-06 14:22:50 -0300

    Deply improvments

--- sentiment_analysis/deploy/remove_service.sh
+++ sentiment_analysis/deploy/remove_service.sh
@@ -4,8 +4,8 @@
 
 # Removing running container
 CONTAINER_ID="$(docker ps -a | grep SENTIMENT_ANALYSIS | awk '{print $1}')"
-docker rm $CONTAINER_ID -f
+docker rm -f $CONTAINER_ID
 
 # Removing current docker imager for the respective service
 IMAGE_ID="$(docker images -a | grep singularitynet/sentiment_analysis | awk '{print $3}'"
-docker rmi $CONTAINER_ID -f
+docker rmi -f $CONTAINER_ID
