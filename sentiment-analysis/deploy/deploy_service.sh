41558e3ea9c7d506eb4284977098e9a4f69a55d0
Glauter Lemos <glautervl@gmail.com>
2018-10-20 00:40:58 -0300

    improvements

--- sentiment-analysis/deploy/deploy_service.sh
+++ sentiment-analysis/deploy/deploy_service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t singularitynet/sentiment_analysis:latest https://raw.githubusercontent.com/glautervl/nlp-services/master/sentiment-analysis/Dockerfile
+docker build -t singularitynet/sentiment_analysis:latest https://raw.githubusercontent.com/singnet/nlp-services/master/sentiment-analysis/Dockerfile
 docker run --name SENTIMENT_ANALYSIS -p 7010:7010 -idt singularitynet/sentiment_analysis:latest
