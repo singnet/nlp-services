2017be71c1713cd277061bf433a570ee92b21467
gontijo <arturgontijo@gmail.com>
2020-01-23 11:31:49 -0300

    Updating repo.

--- cntk-language-understanding/buildproto.sh
+++ cntk-language-understanding/buildproto.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/language_understanding.proto
+/root/anaconda3/envs/cntk-py35/bin/python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/language_understanding.proto
